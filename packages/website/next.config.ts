cf68746b3c377b5cd968aa1d034ad1f25d4b769c
赵玉婷 <lipengyu2006@gmail.com>
2025-03-11 21:03:45 +0800

    /

--- packages/website/next.config.ts
+++ packages/website/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   basePath: '/tai',
-  assetPrefix: '/tai/',
+  assetPrefix: '/tai',
   images: {
     remotePatterns: [
       {
