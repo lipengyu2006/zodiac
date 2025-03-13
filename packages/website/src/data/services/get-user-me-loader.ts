5cb1fd7e12ddf352bb21ff5b7916116ac11b6d4f
赵玉婷 <lipengyu2006@gmail.com>
2025-03-13 20:12:50 +0800

    NODE_ENV=production && NEXT_PHASE=phase-production-build

--- packages/website/src/data/services/get-user-me-loader.ts
+++ packages/website/src/data/services/get-user-me-loader.ts
@@ -6,6 +6,12 @@ const query = qs.stringify({
 })
 
 export async function getUserMeLoader() {
+  if (
+    process.env.NODE_ENV === 'production' &&
+    process.env.NEXT_PHASE === 'phase-production-build'
+  ) {
+    return { ok: true, data: { id: 'mock', image: null }, error: null } // 模拟数据
+  }
   return await apiCall('/api/users/me', {
     method: 'GET',
     cache: 'no-cache',
